38e795b47bc265542bc2a9b997f43e20a77d6209
yang <yueyangyu42@gmail.com>
2018-07-07 17:38:28 -0700

    fourth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import myImage from "./engage.jpg";
+
+const AppHolder = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  background-color: black;
+  margin: 0px;
+  padding: 0px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0px;
+  padding: 0px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  width: 300px;
+  height: 300px;
+`;
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <AppHolder>
+        <Container>
+          <Logo src={myImage} />
+        </Container>
+      </AppHolder>
+    );
   }
 }
 
